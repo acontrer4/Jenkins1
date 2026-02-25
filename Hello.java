ebebb5906925f60f734cbc2aaa01dc3847612cdc
acontrer4 <acontrer@mtu.edu>
2026-02-24 23:09:25 -0500

    Update Hello.java runtime error

--- Hello.java
+++ Hello.java
@@ -1,5 +1,6 @@
-public class Helo {
+public class Hello {
   public static void main(String[] args) {
-    System.out.println("Hello, Jenkins!");
+      String s = null;
+    System.out.println(s.length());
   }
 }
